12c1e714f0406549e144aec1ee047998b59568c0
ola-cpu <Hackerdetoday@gmail.com>
2020-11-23 16:30:40 +0100

    une correction lors de la suppresion des fichier

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,20 +1,19 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html >
+<html lang="{{ str_replace('_','-',app()->getLocale()) }}">
+<head>
+<meta charset="utf-8">
+<meta name ="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello city </title>
+<title>Hello city </title>
 
-        
-    </head>
-    <body >
-        <h1>hello les bros je fais du laravel</h1>
+</head>
+<body>
+ <h1> hello city je debut en laravel</h1>
 
-        <p>  it's currently {{ date('h:i A')}} </p>
+ <p>  l'heure actuel est {{ date('h:i A')}} .</p>
 
-        <footer>
-        <p> &copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About Us</a>/p>
-        </footer>
-    </body>
-</html>
+ <footer>
+ <p> &copy; Copyright {{date('y')}} &middot; <a href="/about-us">About-Us </a> </p>
+ </footer>
+ </body>
+ </html>
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,20 +1,19 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html >
+<html lang="{{ str_replace('_','-',app()->getLocale()) }}">
+<head>
+<meta charset="utf-8">
+<meta name ="viewport" content="width=device-width, initial-scale=1">
 
-        <title>About Us | Hello city </title>
+<title>About Us |Hello city </title>
 
-        
-    </head>
-    <body >
-       
-        <p> je fais du laravel seul ohh  &hearts; by ouroboros;  </p>
-        <p><a href="/"> home </a></p>
+</head>
+<body>
+ <h1> je suis le developeur ouroboros &hearts;</h1>
 
-        <footer>
-        <p> &copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About Us</a>/p>
-        </footer>
-    </body>
-</html>
+<p> retour a la page d'accuile  <a href="/">Home </a></p>
+
+ <footer>
+ <p> &copy; Copyright {{date('y')}} &middot; <a href="/about-us">About Us </a> </p>
+ </footer>
+ </body>
+ </html>
