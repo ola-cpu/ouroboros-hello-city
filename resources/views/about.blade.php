e0a5cff69c5a2af79e83b0ff5f3288561eddc7f9
ola-cpu <Hackerdetoday@gmail.com>
2020-11-23 14:29:22 +0100

    ajout de la page abouts avec des routes specifier

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,14 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello city </title>
+        <title>About Us | Hello city </title>
 
         
     </head>
     <body >
-        <h1>hello les bros je fais du laravel</h1>
-
-        <p>  it's currently {{ date('h:i A')}} </p>
+       
+        <p> je fais du laravel seul ohh  &hearts; by ouroboros;  </p>
+        <p><a href="/"> home </a></p>
 
         <footer>
         <p> &copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About Us</a>/p>
