613991ad7c5c55023e058bfbd0fe8b12ce38c19b
ola-cpu <Hackerdetoday@gmail.com>
2020-11-28 16:32:05 +0100

    creaction et utilisation des variable de configuration

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,13 +4,12 @@
 <meta charset="utf-8">
 <meta name ="viewport" content="width=device-width, initial-scale=1">
 
-<title>@yield('title')</title>
+<title>@yield('title',config('app.name') )</title>
 
 </head>
 <body>
 
 @yield('content')
-	
 	
 
  <footer>
